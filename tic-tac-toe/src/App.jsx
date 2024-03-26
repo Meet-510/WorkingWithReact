20938852878c9c3bbd30b6297675d3af3ada315f
Meet-510 <mistrymeet941@gmail.com>
2024-03-26 17:00:50 +0530

    Initial

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -38,10 +38,8 @@ function App() {
             isActive={activePlayer === "O"}
           />
         </ol>
-        <GameBoard
-          onSelectSquare={handleSelectSquare}
-          activePlayerSymbol={activePlayer}
-        />
+        <GameBoard onSelectSquare={handleSelectSquare} />
+        turns={gameTurns}
       </div>
       <Log />
     </main>

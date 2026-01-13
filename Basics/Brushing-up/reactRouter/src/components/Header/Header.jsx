30687f46f7dcd0b25e5887a9ea03fcaa3ca63f1d
Meet-510 <mistrymeet941@gmail.com>
2026-01-12 22:43:08 -0700

    router

--- Basics/Brushing-up/reactRouter/src/components/Header/Header.jsx
+++ Basics/Brushing-up/reactRouter/src/components/Header/Header.jsx
@@ -33,6 +33,7 @@ export default function Header() {
             <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
               <li>
                 <NavLink
+                  to="/"
                   className={({ isActive }) =>
                     `block py-2 pr-4 pl-3 duration-200
                   ${
